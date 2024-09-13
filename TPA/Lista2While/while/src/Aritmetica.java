1e9af8806601780ac8d682b9d54de482feb72717
Bragaaxz <leonardo.bragap13@gmail.com>
2024-09-13 07:12:50 -0300

    Atualizar o Aritmetica.java

--- TPA/Lista2While/while/src/Aritmetica.java
+++ TPA/Lista2While/while/src/Aritmetica.java
@@ -1,6 +1,5 @@
 package whil;
-import java.util.Scanner;
-	import java.util.Scanner;
+import java.util.Scanner;
 	public class Aritmetica {
 	    public static void main(String[] args) {
 	        Scanner ler = new Scanner(System.in);
