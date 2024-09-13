f409053a5144e852d225218c38c5699bb51231bb
Bragaaxz <leonardo.bragap13@gmail.com>
2024-09-13 09:27:07 -0300

    Update Peso.java

--- TPA/ListaDoWhile/lacodo/src/lacodo/Peso.java
+++ TPA/ListaDoWhile/lacodo/src/lacodo/Peso.java
@@ -6,22 +6,22 @@ public static void main(String[] args) {
 	String resultado,continuacao;
 	double peso,alt,pesoid,pesu;
 	do {
-		System.out.println("Digite  seu Gênero 'f' para Feminino e 'm' para Masculinino :");	
+		System.out.println("Digite  seu GÃªnero 'f' para Feminino e 'm' para Masculinino :");	
 	resultado = ler.next();
 		System.out.println("Digite seu peso : ");
 	peso = ler.nextDouble();
-		System.out.println("Digite sua Altura em cm :");
+		System.out.println("Digite sua Altura em cm : ");
 	alt = ler.nextDouble();	
 if (resultado.equalsIgnoreCase("f")) {
 	pesoid = 52 + (0.67 * (alt - 152.4));
 	pesu = pesoid - peso;
-	System.out.println("Esse é seu peso ideal : " + pesoid  + "A diferença dos peso é de :" + pesu + "KG");
+	System.out.println("Esse Ã© seu peso ideal : " + pesoid  + " A diferenÃ§a dos peso Ã© de : " + pesu + " KG");
 } else{
 	pesoid = 52 + (0.75 * (alt - 152.4));
 	pesu = pesoid - peso;
-	System.out.println("Esse é seu peso ideal : " + pesoid + "A diferença dos peso é de :" + pesu + "KG" );
+	System.out.println("Esse Ã© seu peso ideal : " + pesoid + " A diferenÃ§a dos peso Ã© de : " + pesu + " KG" );
 			}
-	System.out.println("voce gostaria de refazer este processo? Digite 'S' para sim e 'N' para não");	
+	System.out.println("voce gostaria de refazer este processo? Digite 'S' para sim e 'N' para nÃ£o");	
 	continuacao = ler.next();
 	if (continuacao.equalsIgnoreCase("N")) {
 		System.out.println("tmj :)");
